304bd00698d799df5bb8a3b43372aecfe30309cf
nytsdayo <nyantsutsu@gmail.com>
2024-02-06 00:40:13 +0900

    get test & post test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,39 @@
 # from django.test import TestCase
+from django.contrib.auth import SESSION_KEY, get_user_model
+from django.test import TestCase
+from django.urls import reverse
 
+User = get_user_model()
+
+
+class TestSignupView(TestCase):
+    def setUp(self):
+        self.url = reverse("accounts:signup")
+
+    def test_success_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "accounts/signup.html")
+
+    def test_success_post(self):
+        valid_data = {
+            "username": "testuser",
+            "email": "test@test.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+        response = self.client.post(self.url, valid_data)
+        # 1の確認 = tweets/homeにリダイレクトすること
+        self.assertRedirects(
+            response,
+            reverse("tweets:home"),
+            status_code=302,
+            target_status_code=200,
+        )
+		# 2の確認 = ユーザーが作成されること
+        self.assertTrue(User.objects.filter(username=valid_data["username"]).exists())
+        # 3の確認 = ログイン状態になること
+        self.assertIn(SESSION_KEY, self.client.session)
 
 # class TestSignupView(TestCase):
 #     def test_success_get(self):
