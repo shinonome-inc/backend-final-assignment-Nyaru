277339a55307b870d0a3ac897175b56af378139a
nytsdayo <nyantsutsu@gmail.com>
2024-02-14 07:23:17 +0900

    Create Login Page (ログイン処理未実装)

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 User = get_user_model()  # こっちで先に変数代入する！
 
@@ -9,7 +9,10 @@ class SignupForm(UserCreationForm):
         model = User  # model = get_user_model() は NG
         fields = ("username", "email")  # Remove trailing whitespace
 
-
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password")  # Remove trailing whitespace
 # password1, password2というフィールドはUserCreationFormの方で設定されているため、
 # fieldsの欄には、Userモデルの中にある、
 # blankにはできない値であるusernameとemailをセットする。
