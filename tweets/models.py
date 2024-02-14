988a6c7f9dc7de092de5316dc2ec567896b32a37
nytsdayo <nyantsutsu@gmail.com>
2024-02-15 00:23:00 +0900

    Create Tweet Model

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,9 @@
-# from django.db import models
+from django.db import models
 
 
-# class Tweet(models.Model):
+class Tweet(models.Model):
+    image = models.ImageField()
+    body = models.TextField(max_length=140)
+    creator = models.CharField(max_length=255)
+    created = models.DateTimeField()
+    likes = models.IntegerField()
