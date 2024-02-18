6ff3d30a21a779eddf60482cd33315dd67b33be6
nytsdayo <nyantsutsu@gmail.com>
2024-02-18 11:20:11 +0900

    一次レビューの反映 その3

--- accounts/models.py
+++ accounts/models.py
@@ -8,12 +8,4 @@ class User(AbstractUser):
     class Meta(AbstractUser.Meta):
         swappable = "AUTH_USER_MODEL"
 
-
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
-
-
-# class User(AbstractUser):
-
-
 # class FriendShip(models.Model):
