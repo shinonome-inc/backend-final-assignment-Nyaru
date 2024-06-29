03b5475e2ce1ed76355e16596e9059e22a07ecb9
nytsdayo <nyantsutsu@gmail.com>
2024-06-29 20:04:17 +0900

    change error message

--- tweets/forms.py
+++ tweets/forms.py
@@ -12,4 +12,11 @@ class TweetCreateForm(ModelForm):
             "image",
             "body",
         )
-        widgets = {"content": forms.Textarea(attrs={"rows": 4, "cols": 35, "placeholder": "いまどうしてる？"})}
+        widgets = {"content": forms.Textarea(attrs={"rows": 4, "cols": 35, "placeholder": "いまどうしているのですわ？"})}
+
+        error_messages = {
+            'body': {
+                'max_length': "ツイートが" + str(Tweet.body.field.max_length) + "字を超えていますわ～！",
+                'required': "ツイート内容がありませんわ～！",
+            },
+        }
