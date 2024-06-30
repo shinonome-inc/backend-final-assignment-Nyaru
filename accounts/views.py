4e5dbe1730cb74852236d8a15b0ac833c5273fbe
nytsdayo <nyantsutsu@gmail.com>
2024-06-30 14:48:54 +0900

    useView TempleteView -> ListView

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, ListView
 
 from tweets.models import Tweet
 
@@ -24,16 +24,22 @@ class SignupView(CreateView):
         return response
 
 
-class UserProfileView(TemplateView, LoginRequiredMixin):
-
+class UserProfileView(ListView, LoginRequiredMixin):
     template_name = "accounts/user_profile.html"
+    model = Tweet
 
-    # 継承元のget_context_dataをオーバーライド（要勉強）
-    def get_context_data(self, **kwargs):
-        # 親クラスのget_context_dataを呼び出して基本的なコンテキストデータを取得
-        context = super().get_context_data(**kwargs)
-        context["tweets"] = Tweet.objects.filter(creator__username=self.kwargs["username"])
-        return context
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset(**kwargs)  # Article.objects.all() と同じ結果
+        queryset = queryset.filter(creator__username=self.kwargs["username"])
+        queryset = queryset.order_by("-created")
+        return queryset
+
+    # # 継承元のget_context_dataをオーバーライド（要勉強）
+    # def get_context_data(self, **kwargs):
+    #     # 親クラスのget_context_dataを呼び出して基本的なコンテキストデータを取得
+    #     context = super().get_context_data(**kwargs)
+    #     context["tweets"] = Tweet.objects.filter(creator__username=self.kwargs["username"])
+    #     return context
 
 
 # class FollowView(LoginRequiredMixin, TemplateView):
